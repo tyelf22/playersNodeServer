1bc733eb5844f5a6fe8d2ea2000487419bf3981b
tyelf22 <tyelf22@hotmail.com>
2020-10-04 16:29:51 -0600

    fixed port settings

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import playersRoutes from './routes/players.js'
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
@@ -13,5 +13,5 @@ app.use('/players', playersRoutes);
 app.get('/', (req, res) => res.send('Hello from Homepage'));
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: http://localhost:${PORT}`);
+    console.log(`Server running on port: ${PORT}`);
 })
